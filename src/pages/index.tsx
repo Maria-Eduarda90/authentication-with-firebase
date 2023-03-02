ee7788691a240312665a7bf8e4d79f682cb842ec
Mary Skywalker <snowdennaruto90@gmail.com>
2023-03-01 20:16:19 -0400

    login page created

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,15 @@
 import type { NextPage } from 'next';
+import Image from 'next/image';
+import { SignIn } from '../components/SignIn';
+
+import styles from './styles.module.scss';
 
 const Home: NextPage = () => {
   return (
-    <h1>Hello</h1>
+    <div className={styles.cotainer}>
+      <Image src="/astronaut.png" alt="astronaut" width={'400'} height={'100'}/>
+      <SignIn/>
+    </div>
   )
 }
 
