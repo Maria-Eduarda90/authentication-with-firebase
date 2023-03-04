918e8689b89ffbdf6d4fdaaaff29f7ef3fc2cffb
Mary Skywalker <snowdennaruto90@gmail.com>
2023-03-03 22:29:48 -0400

    context added around the application

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,13 @@
 import '../styles/globals.scss';
+import { AuhtProvider } from '../context/AuthContext';
 import type { AppProps } from 'next/app';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return(
+    <AuhtProvider>
+      <Component {...pageProps} />
+    </AuhtProvider>
+  );
 }
 
 export default MyApp
