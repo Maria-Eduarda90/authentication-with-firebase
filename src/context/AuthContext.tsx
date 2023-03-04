11405902c377a723668aafe44921a147eee5f57a
Mary Skywalker <snowdennaruto90@gmail.com>
2023-03-04 18:40:46 -0400

    modified

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,14 +1,21 @@
-import { createContext, ReactNode, useEffect, useState } from "react";
+import { createContext, ReactNode, useState } from "react";
+import { UserType } from "../@types/User";
+
+type AuthContextType = {
+    user: UserType | null | undefined;
+}
 
 type ElementChildren = {
     children: ReactNode;
 }
 
-export const AuthContext = createContext({});
+export const AuthContext = createContext({} as AuthContextType);
 
 export function AuhtProvider({ children }: ElementChildren){
+    const [user, setUser] = useState<UserType | null>();
+
     return(
-        <AuthContext.Provider value={'teste'}>
+        <AuthContext.Provider value={{ user }}>
             {children}
         </AuthContext.Provider>
     )
