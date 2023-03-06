c77adf7a807060df44db1872b61342cb4425138b
Mary Skywalker <snowdennaruto90@gmail.com>
2023-03-05 22:57:58 -0400

    rendering

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,27 @@
 import '../styles/globals.scss';
 import { AuhtProvider } from '../context/AuthContext';
 import type { AppProps } from 'next/app';
+import { useEffect, useState } from 'react';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return(
-    <AuhtProvider>
-      <Component {...pageProps} />
-    </AuhtProvider>
-  );
+  const [showChild, setShowChild] = useState(false);
+  useEffect(() => {
+    setShowChild(true);
+  }, []);
+
+  if (!showChild) {
+    return null;
+  }
+
+  if (typeof window === 'undefined') {
+    return <></>;
+  } else {
+    return (
+      <AuhtProvider>
+        <Component {...pageProps} />
+      </AuhtProvider>
+    );
+  }
 }
 
 export default MyApp
