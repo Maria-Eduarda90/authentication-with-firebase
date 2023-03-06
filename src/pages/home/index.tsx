39b0f5083f48c39bda7b25543d676acd86d4df0a
Mary Skywalker <snowdennaruto90@gmail.com>
2023-03-05 22:57:08 -0400

    private routes

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,5 +1,29 @@
+import { GetServerSideProps } from "next";
+import { parseCookies } from "nookies";
+import { useContext } from "react";
+import { AuthContext } from "../../context/AuthContext";
+
 export default function Home(){
+    const { user } = useContext(AuthContext);
     return(
-        <h1>Hello word!</h1>
+        <h1>Hello word!{user.email}</h1>
     );
+}
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+    const { ['auth']: token } = parseCookies(ctx);
+    console.log('token:', token);
+
+    if (!token) {
+        return {
+            redirect: {
+                destination: '/',
+                permanent: false,
+            }
+        }
+    }
+
+    return {
+        props: {}
+    }
 }
